092f10e6c94a9c2ff905f96d52651460f02c5a9c
Alan <love@inheart.club>
2019-05-28 10:51:40 +0800

    修改web.xml配置文件

--- drp-web/src/main/java/com/yootk/drp/dao/impl/MemberDAOImpl.java
+++ drp-web/src/main/java/com/yootk/drp/dao/impl/MemberDAOImpl.java
@@ -1,5 +1,6 @@
 package com.yootk.drp.dao.impl;
 
+import com.yootk.common.annotation.Repository;
 import com.yootk.common.dao.abs.AbstractDAO;
 import com.yootk.drp.dao.IMemberDAO;
 import com.yootk.drp.vo.Member;
@@ -9,15 +10,16 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Set;
 
+@Repository
 public class MemberDAOImpl extends AbstractDAO implements IMemberDAO {
     @Override
-    public boolean doEditMessage(String mid,String name,String phone,String email) throws SQLException {
+    public boolean doEditMessage(String mid, String name, String phone, String email) throws SQLException {
         String sql = "UPDATE member SET name=?,phone=?,email=? WHERE mid=?";
         super.pstmt = super.conn.prepareStatement(sql);
-        super.pstmt.setString(1,name);
-        super.pstmt.setString(2,phone);
-        super.pstmt.setString(3,email);
-        super.pstmt.setString(4,mid);
+        super.pstmt.setString(1, name);
+        super.pstmt.setString(2, phone);
+        super.pstmt.setString(3, email);
+        super.pstmt.setString(4, mid);
         return super.pstmt.executeUpdate() > 0;
     }
 
@@ -25,20 +27,20 @@ public class MemberDAOImpl extends AbstractDAO implements IMemberDAO {
     public String findByPassword(String mid) throws SQLException {
         String sql = "SELECT password FROM member WHERE mid =? ";
         super.pstmt = super.conn.prepareStatement(sql);
-        super.pstmt.setString(1,mid);
+        super.pstmt.setString(1, mid);
         ResultSet rs = super.pstmt.executeQuery();
-        if(rs.next()){
+        if (rs.next()) {
             return rs.getString(1);
         }
         return null;
     }
 
     @Override
-    public boolean doEditPassword(String mid,String password) throws SQLException {
+    public boolean doEditPassword(String mid, String password) throws SQLException {
         String sql = "UPDATE member SET password=? WHERE mid=?";
         super.pstmt = super.conn.prepareStatement(sql);
-        super.pstmt.setString(1,password);
-        super.pstmt.setString(2,mid);
+        super.pstmt.setString(1, password);
+        super.pstmt.setString(2, mid);
         return super.pstmt.executeUpdate() > 0;
     }
 
@@ -59,7 +61,10 @@ public class MemberDAOImpl extends AbstractDAO implements IMemberDAO {
 
     @Override
     public Member findById(String s) throws SQLException {
-        return null;
+        String sql = "SELECT mid,lid,did,name,sal,phone,password,photo,note,regdate,inmid,locked,type,email,cuid, FROM member WHERE mid=?";
+        super.pstmt = super.conn.prepareStatement(sql);
+        super.pstmt.setString(1, s);
+        return super.handleResultToVO(super.pstmt.executeQuery(), Member.class);
     }
 
     @Override
--- drp-web/src/main/java/com/yootk/drp/service/front/impl/MemberServiceFrontImpl.java
+++ drp-web/src/main/java/com/yootk/drp/service/front/impl/MemberServiceFrontImpl.java
@@ -1,6 +1,7 @@
 package com.yootk.drp.service.front.impl;
 
 import com.yootk.common.annotation.Autowired;
+import com.yootk.common.annotation.Service;
 import com.yootk.common.service.abs.AbstractService;
 import com.yootk.drp.dao.IMemberDAO;
 import com.yootk.drp.service.front.IMemberServiceFront;
@@ -8,27 +9,34 @@ import com.yootk.drp.vo.Member;
 
 import java.sql.SQLException;
 
+@Service
 public class MemberServiceFrontImpl extends AbstractService implements IMemberServiceFront {
-    @Override
-    public boolean login(Member vo) throws SQLException {
-        return false;
-    }
     @Autowired
     private IMemberDAO memberDAO;
 
     @Override
+    public boolean login(Member vo) throws SQLException {
+        Member member = memberDAO.findById(vo.getMid());
+        if (member != null) {
+            return member.getPassword().equals(vo.getPassword());
+        }
+        return false;
+    }
+
+
+    @Override
     public boolean editMessage(String mid, String name, String phone, String email) throws Exception {
-        if(name =="" ||name.length()==0||phone =="" ||phone.length()==0||email =="" ||email.length()==0){
+        if (name == "" || name.length() == 0 || phone == "" || phone.length() == 0 || email == "" || email.length() == 0) {
             return false;
         }
         return memberDAO.doEditMessage(mid, name, phone, email);
     }
 
     @Override
-    public boolean editPassword(String mid, String oldPassword,String newPassword) throws Exception {
-        if(!(oldPassword=="" ||oldPassword.length()==0 ||oldPassword=="" ||oldPassword.length()==0)){
-            if(memberDAO.findByPassword(mid) == oldPassword){
-                return memberDAO.doEditPassword(mid,newPassword);
+    public boolean editPassword(String mid, String oldPassword, String newPassword) throws Exception {
+        if (!(oldPassword == "" || oldPassword.length() == 0 || oldPassword == "" || oldPassword.length() == 0)) {
+            if (memberDAO.findByPassword(mid) == oldPassword) {
+                return memberDAO.doEditPassword(mid, newPassword);
             }
         }
         return false;
