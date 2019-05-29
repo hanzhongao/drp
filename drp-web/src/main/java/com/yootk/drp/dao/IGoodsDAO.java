4a35649b05aaf46a9a4888223bcf88bf87844376
LiWangRan <1012441885@qq.com>
2019-05-30 01:23:19 +0800

    更改bug

--- drp-web/src/main/java/com/yootk/drp/dao/IGoodsDAO.java
+++ drp-web/src/main/java/com/yootk/drp/dao/IGoodsDAO.java
@@ -1,12 +1,16 @@
 package com.yootk.drp.dao;
 
+import com.yootk.common.dao.IBaseDAO;
 import com.yootk.drp.vo.Goods;
 
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Set;
 
-public interface IGoodsDAO {
+/**
+ * @author 再次BUG更改
+ */
+public interface IGoodsDAO extends IBaseDAO<Long, Goods>  {
     /**
      * 根据已有的商品编号的信息查询出对应的所有商品数据
      * @param gids  商品编号
--- drp-web/src/main/java/com/yootk/drp/dao/impl/GoodsDAOImpl.java
+++ drp-web/src/main/java/com/yootk/drp/dao/impl/GoodsDAOImpl.java
@@ -9,6 +9,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+
+/**
+ * @author 再次BUG更改
+ */
 @Repository
 public class GoodsDAOImpl extends AbstractDAO implements IGoodsDAO {
     @Override
@@ -21,5 +25,52 @@ public class GoodsDAOImpl extends AbstractDAO implements IGoodsDAO {
         sql.delete(sql.length()-1,sql.length()).append(")") ;
         super.pstmt = super.conn.prepareStatement(sql.toString()) ;
         return super.handleResultToList(super.pstmt.executeQuery(),Goods.class);
+    }
+
+    @Override
+    public boolean doCreate(Goods goods) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean doEdit(Goods goods) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean doRemove(Set<Long> longs) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public Goods findById(Long aLong) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<Goods> findAll() throws SQLException {
+        String sql ="SELECT gid,name,wiid,stid,price,weight,photo,note,lastin,stornum,recorder,FROM goods Where delflag=0";
+        super.pstmt = super.conn.prepareStatement(sql);
+        return super. handleResultToList(super.pstmt.executeQuery(),Goods.class);
+    }
+
+    @Override
+    public List<Goods> findSplit(Long currentPage, Integer lineSize) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<Goods> findSplit(Long currentPage, Integer lineSize, String column, String keyWord) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Long getAllCount() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Long getAllCount(String column, String keyWord) throws SQLException {
+        return null;
     }
 }
