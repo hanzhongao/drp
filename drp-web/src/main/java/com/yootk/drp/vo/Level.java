5b3380bb00375fc1f7f0c2ce3c93a1c82cee2f81
hza <hanzhongao@outlook.com>
2019-05-31 00:16:11 +0800

    添加title的getter、setter方法

--- drp-web/src/main/java/com/yootk/drp/vo/Level.java
+++ drp-web/src/main/java/com/yootk/drp/vo/Level.java
@@ -20,6 +20,14 @@ public class Level implements Serializable {
         this.lid = lid;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     @Override
     public String toString() {
         return "Level{" +
